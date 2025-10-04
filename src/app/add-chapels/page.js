a3813fe7f0c154cace6952f14afe13a830189c20
Thalles Lorrhan <tahlles.lorrhan30@gmail.com>
2025-10-04 20:13:10 -0300

    button add 1.1

--- src/app/add-chapels/page.js
+++ src/app/add-chapels/page.js
@@ -1,11 +1,15 @@
 "use client";
+export const dynamic = "force-dynamic";
 
+import { Suspense } from "react";
 import AddChapel from "@/components/AddChapel ";
 
 export default function AddChapelPage() {
   return (
     <div>
-      <AddChapel />
+      <Suspense fallback={<div>Carregando...</div>}>
+        <AddChapel />
+      </Suspense>
     </div>
   );
 }
